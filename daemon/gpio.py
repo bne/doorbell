0548e4be74403b29ffad8336c130069e30fd1659
Ben Miller <ben@hyl.co.uk>
2012-11-11 13:48:17 +0000

    Plumbs in GPIO gubbins to daemon

--- daemon/gpio.py
+++ daemon/gpio.py
@@ -9,21 +9,37 @@ Mock events
 """
 from struct import unpack
 
+try:
+    import RPi.GPIO as GPIO
+except ImportError:
+    GPIO = None
+
 EVT_DOORBELL = 1
 EVT_DOOROPEN = 2
 
 class GPIOListener(object):
 
     def __init__(self):
-        # event3 == laptop keyboard
-        self.port = open('/dev/input/event3', 'rb') 		
+        if GPIO:
+            GPIO.setmode(GPIO.BOARD)
+            GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+            GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        else:
+            # event3 == laptop keyboard
+            self.port = open('/dev/input/event3', 'rb') 		
 
     def listen(self):
-        evt = unpack('4B', self.port.read(4))
-        if evt[2] == 32:
-            return EVT_DOORBELL
-        if evt[2] == 19:
-            return EVT_DOOROPEN
+        if GPIO:
+            if not GPIO.input(22):
+                return EVT_DOOROPEN                
+            if not GPIO.input(7):
+                return EVT_DOORBELL
+        else:
+            code = unpack('4B', self.port.read(4))[2]
+            if code == 32:
+                return EVT_DOORBELL
+            if code == 19:
+                return EVT_DOOROPEN
 
 if __name__ == '__main__':
     gl = GPIOListener()
