0c0ad72df0c1ea07d964d987142e6959e837595c
Ben Miller <ben@hyl.co.uk>
2016-04-29 15:48:29 +0100

    mustache

--- webserver/app/static/js/kiosk.js
+++ webserver/app/static/js/kiosk.js
@@ -1,5 +1,5 @@
 
-var weather_icon_map = {
+var weatherIcons = {
     '01d': 'wi-day-sunny',        '01n': 'wi-night-clear', //clear sky
     '02d': 'wi-day-cloudy-high',  '02n': 'wi-night-cloudy-high', //few clouds
     '03d': 'wi-day-cloudy',       '03n': 'wi-night-alt-cloudy', //scattered clouds
@@ -10,11 +10,33 @@ var weather_icon_map = {
     '13d': 'wi-day-snow',         '13n': 'wi-night-alt-snow', //snow
     '50d': 'wi-day-haze',         '50n': 'wi-night-fog' //mist
 }
+var monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+var dayNames = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+
+function lz(i) {
+    var s = '0'+i;
+    return s.substr(s.length-2);
+}
+
+function nth(d) {
+  if(d>3 && d<21) { return 'th'; }
+  switch (d % 10) {
+        case 1:  return "st";
+        case 2:  return "nd";
+        case 3:  return "rd";
+        default: return "th";
+    }
+}
 
 function updateClock() {
+    var now = new Date();
     $('#clock')
-    .html(window.templates['clock']({
-        now: moment()
+    .html(Mustache.render(window.templates['clock'], {
+        time: lz(now.getHours()) +':'+ lz(now.getMinutes()),
+        day: dayNames[now.getDay()],
+        date: now.getDate() + nth(now.getDate()),
+        month: monthNames[now.getMonth()],
+        year: now.getFullYear()
     }));
     setTimeout(updateClock, 1000);
 }
@@ -27,11 +49,12 @@ function updateWeather() {
 
     $.get(url)
     .done(function(data) {
+        console.log(data)
         $('#weather')
-        .html(window.templates['weather']({
+        .html(Mustache.render(window.templates['weather'], {
             temp: Math.round(data['main']['temp']),
             description: data['weather'][0]['description'],
-            icon: weather_icon_map[data['weather'][0]['icon']]
+            icon: weatherIcons[data['weather'][0]['icon']]
         }));
     });
 
@@ -39,10 +62,13 @@ function updateWeather() {
 }
 
 $(function() {
-    window.templates = {
-        clock: _.template($('#tmpl-clock').html()),
-        weather: _.template($('#tmpl-weather').html())
-    }
-    updateClock();
-    updateWeather();
+    window.templates = {};
+    $.get('/static/templates/kiosk/clock.mst', function(template) {
+        window.templates['clock'] = template;
+        updateClock();
+    });
+    $.get('/static/templates/kiosk/weather.mst', function(template) {
+        window.templates['weather'] = template;
+        updateWeather();
+    });
 });
